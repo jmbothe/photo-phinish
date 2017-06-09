328ffd16a78b262573a6526c617102961ba1b0a6
Jeff Bothe <jeffrymbothe@gmail.com>
2017-06-09 16:40:59 -0500

    fine tune 07

--- js/app.js
+++ js/app.js
@@ -1,8 +1,1 @@
 $(document).foundation()
-
-jQuery(document).ready(function($) {
-     $(".scroll").click(function(event){
-          event.preventDefault();
-          $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1500);
-     });
-});
